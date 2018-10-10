ffe490df963da4d3749aff0b602e7ea93480b904
thms317 <brouwer.thomas@gmail.com>
2018-10-10 14:57:16 +0200

    Read pars from folder (LMST data)

--- batch_analysis_datx.py
+++ batch_analysis_datx.py
@@ -9,11 +9,11 @@ def default_pars():
     pars = {}
     pars['kT'] = 4.114  # pN nm
     pars['L0'] = 0.34  # nm / base pair
-    pars['L_bp'] = 5169  # number of base pairs
+    pars['L_bp'] = 5137  # number of base pairs
     pars['P_nm'] = 50  # persistence length
     pars['S_pN'] = 1000  # stretch modulus
     pars['z0_nm'] = 0  # offset in nm / subunit
-    pars['NRL'] = 195  # nucleosome repeat length
+    pars['NRL'] = 193  # nucleosome repeat length
     pars['repeats'] = 16  # number of repeats
     pars['type'] = "human"  # type of histone
     pars['NRL_str'] = str(pars['NRL'])+'x'+str(pars['repeats'])+'_'+pars['type']  # Nucleosome Repeat Length + #repeats
@@ -28,8 +28,8 @@ p = default_pars()
 
 plt.close("all")
 
-table_path = "C:\\Users\\tbrouwer\\Desktop\\Data\\180924\\"
-table_file = "180924_195"
+table_path = "C:\\Users\\brouw\\Desktop\\Data\\180926\\"
+table_file = "180926_193"
 
 measurements = ba.build_measurements(table_path, table_file + ".datx", p)
 drift_arr = []
--- batch_analysis_fitfiles.py
+++ batch_analysis_fitfiles.py
@@ -9,12 +9,12 @@ def default_pars():
     pars = {}
     pars['kT'] = 4.114  # pN nm
     pars['L0'] = 0.34  # nm / base pair
-    pars['L_bp'] = 5153-16-16  # number of base pairs
+    pars['L_bp'] = 7393  # number of base pairs
     pars['P_nm'] = 50  # persistence length
     pars['S_pN'] = 1000  # stretch modulus
     pars['z0_nm'] = 0  # offset in nm / subunit
-    pars['NRL'] = 192  # nucleosome repeat length
-    pars['repeats'] = 16  # number of repeats
+    pars['NRL'] = 167  # nucleosome repeat length
+    pars['repeats'] = 32  # number of repeats
     pars['type'] = "human"  # type of histone
     pars['NRL_str'] = str(pars['NRL'])+'x'+str(pars['repeats'])+'_'+pars['type']  # Nucleosome Repeat Length + #repeats
     pars['drift'] = []
@@ -26,7 +26,7 @@ def default_pars():
 p = default_pars()
 
 # fitfile_path = "S:\\Brouwer\\Chromatin Force Spectroscopy\\Cummulative\\171x16\\"
-fitfile_path = "C:\\Users\\tbrouwer\\Desktop\\Data\\fitfiles\\"
+fitfile_path = "C:\\Users\\brouw\\Desktop\\Data\\"
 
 plot_rot = True
 
